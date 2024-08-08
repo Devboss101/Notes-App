9499ee5a3af9fbe70c5578dcb9032f870f85822f
Devboss101 <108057188+Devboss101@users.noreply.github.com>
2024-08-08 15:44:53 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -59,11 +59,16 @@ class NotesUI {
         this.note_edit = note_edit
 
         this.root.innerHTML = `
-            <aside>
 
-            <header>
+            <div class="svg_div">
+                <svg class="icon bar activeIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+                <svg class="icon times" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+            </div>
+
+            <aside class="aside_bar">
+
+            <header>    
                 <h1 id="title">Notes</h1>
-
                 <button class="add_btn">Add Notes</button>
             </header>
 
@@ -78,6 +83,7 @@ class NotesUI {
             </aside>
 
             <div class="main">
+                
                 <input type="text" placeholder="Note Title..." id="inpTitle" class="inpTitle">
                 <textarea name="body" id="inpBody" class="inpBody">Take Note...</textarea>
             </div> 
@@ -85,8 +91,17 @@ class NotesUI {
 
         this.root.querySelector('.add_btn').addEventListener('click', () => {
             this.note_add()
+            this.update_active_page(this.root.querySelector('.bar'))
         })
-        
+
+        this.root.querySelectorAll(".icon").forEach((icon, index) => {
+            icon.addEventListener('click', () => {
+               if(icon.classList.contains("activeIcon")){
+                    this.update_svg_icon(index)
+               }
+               
+            })
+        })
         
         const inpTitle = this.root.querySelector('.inpTitle')
         const inpBody = this.root.querySelector('.inpBody')
@@ -136,6 +151,7 @@ class NotesUI {
         note_list_wrapper.querySelectorAll(".note_list_item").forEach(item => {
             item.addEventListener('click', () => {
                 this.note_selected(item.dataset.noteId)
+                this.update_active_page(this.root.querySelector(".bar"))
             })
 
         
@@ -181,6 +197,32 @@ class NotesUI {
         } else {
             note_list_wrapper.classList.remove('empty_wrapper')
             this.root.querySelector('.main').classList.remove('visibility')
+        }
+    }
+
+    update_svg_icon(index) {
+
+        const icons = this.root.querySelectorAll('.icon')
+        icons.forEach(icon => {
+            icon.classList.add("activeIcon")    
+        })
+
+        icons[index].classList.remove('activeIcon')
+
+        console.log(index)
+
+        this.update_active_page(icons[index])
+    }
+
+    update_active_page(icon){
+        if (icon.classList.contains('times')){
+            this.root.querySelector('.aside_bar').classList.remove("inactive")
+            this.root.querySelector('.main').classList.remove('activePage')
+        } else {
+            this.root.querySelector('.aside_bar').classList.add("inactive")
+            this.root.querySelector('.main').classList.add('activePage')
+            this.root.querySelector('.times').classList.add("activeIcon")
+            console.log('bar')
         }
     }
 
